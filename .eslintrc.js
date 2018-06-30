6d2bff1ce9048d750ec2dd4d6c13516dc894db0f
Inori <337938188@qq.com>
2018-06-30 09:59:05 +0800

    可以工作

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,9 @@ module.exports = {
     browser: true
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
+  extends: [
+    'standard',
+  ],
   // required to lint *.wpy files
   plugins: [
     'html'
